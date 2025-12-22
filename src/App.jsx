d9b35402a7ea31236a2470a7109b6a17dd81ac38
oluwafemix <samuelkeller47@gmail.com>
2025-12-22 22:47:13 +0100

    Adding a smooth scroll behavior for smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 function App() {
   return (
+    <html className="scroll-smooth">
     <div className="bg-linear-to-r from-white to-blue-300 min-h-screen text-white">
       <div className="py-4">
       <Navbar />
       </div>
       <Home />
     </div>
+    </html>
   );
 }
 export default App;
