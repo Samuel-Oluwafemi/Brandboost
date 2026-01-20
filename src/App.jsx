dc80d224d0dd0dea9173c1272cabdcc8efb4dc37
oluwafemix <samuelkeller47@gmail.com>
2026-01-20 15:41:19 +0100

    Adding new features

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 function App() {
   return (
-    <html className="scroll-smooth">
+    <html classpm Name="scroll-smooth">
     <div className="bg-linear-to-r from-white to-blue-300 min-h-screen text-white">
       <div className="py-4">
       <Navbar />
